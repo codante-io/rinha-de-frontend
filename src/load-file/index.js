6e601b65f3262e0d7c35a58b51949c640764efbe
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-10-13 09:06:03 -0300

    feat: init json parse to tree viewer

--- src/load-file/index.js
+++ src/load-file/index.js
@@ -6,6 +6,15 @@ loadJsonFile.addEventListener('change', (e) => {
 
   const invalidFile = file.type !== "application/json"
   if (invalidFile) showErrorMessage();
+
+  const reader = new FileReader()
+  reader.readAsText(file)
+  reader.onload = function() {
+    showJsonTreeViewer(file.name, reader.result)
+  }
+  reader.onerror = function() {
+    showErrorMessage()
+  }
 })
 
 function showErrorMessage() {
@@ -15,4 +24,21 @@ function showErrorMessage() {
   filefield.classList.add('filefield--invalid')
   input.setAttribute('aria-describedby', "invalid-file-error")
   input.setAttribute('aria-invalid', "true")
+}
+
+function showJsonTreeViewer(filename, json) {
+  const content = document.querySelector('.content')
+  const jsonTreeViewer = document.querySelector("#json-tree-viewer")
+  const jsonFileName = document.querySelector("#json-file-name")
+
+  content.classList.remove('content--presentation')
+  content.classList.add('content--treeviewer')
+  
+  jsonFileName.innerHTML = filename
+  jsonTreeViewer.innerHTML = parseJson(json)
+}
+
+function parseJson(json) {
+  console.log(JSON.parse(json))
+  return JSON.stringify(json)
 }
